0b5fb551388c4b931852a37038b62ae2f1519b94
Khubaib <khubaib.rafique@ceative.co.uk>
2023-02-13 21:45:23 +0500

    changes added

---
+++ src/data/chat.ts
@@ -0,0 +1,10 @@
+export const chats = [
+  {
+    _id: "63e1439ba8fb0343e10e750k",
+    chatType: "personal",
+    connectedUsers: [
+      '63e1439ba8fb0343e10e74a2', // alice
+      '63e1439ba8fb0343e10e749f' // loop
+    ]
+  },
+]
--- src/data/users.ts
+++ src/data/users.ts
@@ -1,26 +1,31 @@
 export const users = [
   {
     _id: "63e1439ba8fb0343e10e749e",
+    avatar: "https://i.pravatar.cc/300",
     username: 'ether',
     password: '123'
   },
   {
     _id: "63e1439ba8fb0343e10e749f",
+    avatar: "https://i.pravatar.cc/300",
     username: 'loop',
     password: '123'
   },
   {
     _id: "63e1439ba8fb0343e10e74a0",
+    avatar: "https://i.pravatar.cc/300",
     username: 'ghost',
     password: '123'
   },
   {
     _id: "63e1439ba8fb0343e10e74a1",
+    avatar: "https://i.pravatar.cc/300",
     username: 'jayjay',
     password: '123'
   },
   {
     _id: "63e1439ba8fb0343e10e74a2",
+    avatar: "https://i.pravatar.cc/300",
     username: 'alice',
     password: '123'
   },
